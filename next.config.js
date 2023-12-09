e255813f6504559c98f95d3a971dd407bcae713b
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-12-10 01:12:34 +0400

    chore: implement axiom and vercel analytics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withAxiom } = require('next-axiom')
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withAxiom({
+  nextConfig,
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { AxiomWebVitals } from 'next-axiom'
 
 import './globals.css'
 
@@ -21,6 +23,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <SpeedInsights />
+        <Analytics />
+        <AxiomWebVitals />
       </body>
     </html>
   )
